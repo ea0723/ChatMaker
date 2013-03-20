70ff1b32e9e547a103300785c6983de105ff9fd3
ea0723 <elenanewton@icloud.com>
2013-03-20 11:52:15 +0800

    update to files based on new page from hackreactor

---
+++ ChatMaker_Parse.js
@@ -0,0 +1,8 @@
+$(window).on("html_loaded", function(e) {
+    $(".submit_button").click(function(e) {
+        
+        // Initialize the Parse SDK!
+        Parse.initialize("PqYWVyEzWT0PxsMEwlgHdBKU55lB7fspwGp2Pehp","wILiSnUIIdSZrITPZFBASr9WX7HU4d3X5d4wzVEe");
+        
+    });
+});
