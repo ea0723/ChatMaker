6338a302fd4cd796d550fb5b4dc6277a152aa007
ea0723 <elenanewton@icloud.com>
2013-03-22 14:41:23 +0800

    working chat.display();

--- ChatMaker_Parse.js
+++
@@ -1,8 +0,0 @@
-$(window).on("html_loaded", function(e) {
-    $(".submit_button").click(function(e) {
-        
-        // Initialize the Parse SDK!
-        Parse.initialize("PqYWVyEzWT0PxsMEwlgHdBKU55lB7fspwGp2Pehp","wILiSnUIIdSZrITPZFBASr9WX7HU4d3X5d4wzVEe");
-        
-    });
-});
---
+++ submit/chatmaker_ean.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	// initialize parse using Parse ea0723 "ChatMaker" (the name of my app in parse) and Javascript API keys
+    Parse.initialize("PqYWVyEzWT0PxsMEwlgHdBKU55lB7fspwGp2Pehp","wILiSnUIIdSZrITPZFBASr9WX7HU4d3X5d4wzVEe");
+
+    
+    
+
+})
